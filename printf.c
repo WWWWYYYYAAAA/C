7d12e44b252d64808935cd90b55d56494fa36f04
76721 <767210874@qq.com>
2022-10-05 19:53:05 +0800

    float

--- printf.c
+++ printf.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <float.h>
 
 int main()
 {
     int a;
+    float b;
     a= 1;
     printf("HhHhHhHh %d\n", a);
+    printf("size: %lu\n", sizeof(b));
+    printf("max: %E\n", FLT_MAX);
+    printf("min: %E\n", FLT_MIN);
+    printf("precision: %d\n", FLT_DIG);
     getchar();
     getchar();
     return 0;
