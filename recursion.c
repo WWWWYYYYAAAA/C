9176458b9fc37f3e17749e3c8a72d76f0c44044d
76721 <767210874@qq.com>
2022-10-06 20:25:52 +0800

    recursion

---
+++ recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define FIRST 1
+#define SECOND 1
+int fibinac(int i)
+{
+    if(i==1)
+        return FIRST;
+    else if(i==2)
+        return SECOND;
+    else
+        return fibinac(i-1) + fibinac(i-2);
+
+}
+
+int main()
+{
+    int i;
+    for(i=1; i<11; i++)
+    {
+        printf("%d\n", fibinac(i));
+    }
+    getchar();
+    getchar();
+    return 0;
+}
